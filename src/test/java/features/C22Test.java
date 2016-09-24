535dc69c15a921ef083a059fcdea346d2124a328
marianasalcedo <msalcedo.inf@gmail.com>
2016-09-24 13:50:52 +0200

    added classes C21 and C22 with their tests

---
+++ src/test/java/features/C22Test.java
@@ -0,0 +1,21 @@
+package features;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class C22Test {
+    private C22 c22;
+    
+    @Before
+    public void before(){
+        c22 = new C22();
+    }
+    
+    @Test
+    public void testMA() {
+        assertEquals("mA", this.c22.mA());
+    }
+
+}
